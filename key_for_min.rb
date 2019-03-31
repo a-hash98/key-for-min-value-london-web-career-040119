096dbad82506f487c844ae2b72d56635654b41fb
Ay Has <ahashim983@yahoo.com>
2019-03-31 20:31:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  current_value = -1
+  current_key = ""
+  name_hash.each do |item,value|
+    if current_value == -1 && current_key = ""
+      current_value = value
+      current_key = item
+    elsif value < current_value
+      current_value = value
+      current_key = item
+    else
+      return
+    end
+  end
+  return current_key
+      
 
 
 end
