51ccfc111ca3b602add5352feada12b91f454a92
Ay Has <ahashim983@yahoo.com>
2019-03-31 20:37:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,10 +8,7 @@ def key_for_min_value(name_hash)
     current_value = -1
     current_key = ""
     name_hash.each do |item,value|
-      if current_value == -1 && current_key = ""
-        current_value = value
-        current_key = item
-      elsif value < current_value
+      if (current_value == -1 && current_key = "") || value < current_value
         current_value = value
         current_key = item
       end
