d3795b8720bc0c088bced13c86c0d66081edd282
Ay Has <ahashim983@yahoo.com>
2019-03-31 20:34:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,17 +4,18 @@
 def key_for_min_value(name_hash)
   if !name_hash
     return nil
-  end
-  current_value = -1
-  current_key = ""
-  name_hash.each do |item,value|
-    if current_value == -1 && current_key = ""
-      current_value = value
-      current_key = item
-    elsif value < current_value
-      current_value = value
-      current_key = item
+  else
+    current_value = -1
+    current_key = ""
+    name_hash.each do |item,value|
+      if current_value == -1 && current_key = ""
+        current_value = value
+        current_key = item
+      elsif value < current_value
+        current_value = value
+        current_key = item
+      end
     end
+    return current_key
   end
-  return current_key
 end
